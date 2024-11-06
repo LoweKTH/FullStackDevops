7cbb76ce53d10f82102213ab8a6523728f6d51e2
LoweKTH <62517085+LoweKTH@users.noreply.github.com>
2024-11-06 11:07:49 +0100

    #1

---
+++ backend/user-ms/src/main/java/com/fullstackdevops/userms/model/User.java
@@ -0,0 +1,23 @@
+package com.fullstackdevops.userms.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+}
