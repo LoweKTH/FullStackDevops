58ab8864fc3c7ad50b387bd15baab3b52c6d6f0d
benle <benle@kth.se>
2024-11-12 23:30:35 +0100

    add patient when creating a patient user

---
+++ backend/doctorstaff-ms/src/main/java/com/fullstackdevops/doctorstaffms/dto/DoctorDto.java
@@ -0,0 +1,20 @@
+package com.fullstackdevops.doctorstaffms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorDto {
+    private Long socialSecurityNumber;
+    private Long userId;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phoneNumber;
+    private String specialty;
+}
