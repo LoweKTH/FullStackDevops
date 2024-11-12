317b2ce3658831006e45cf6106bc6cee3c55f650
LoweKTH <62517085+LoweKTH@users.noreply.github.com>
2024-11-12 23:57:23 +0100

    update frontend

---
+++ backend/doctorstaff-ms/src/main/java/com/fullstackdevops/doctorstaffms/DoctorstaffMsApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackdevops.doctorstaffms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorstaffMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorstaffMsApplication.class, args);
+    }
+
+}
---
+++ backend/doctorstaff-ms/src/test/java/com/fullstackdevops/doctorstaffms/DoctorstaffMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackdevops.doctorstaffms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorstaffMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
