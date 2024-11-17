60379dd52bdf96e6f8d4e95cabb938ee5cf4d06f
benle <benle@kth.se>
2024-11-17 22:17:52 +0100

    added getStaffForPatients and getPatientsforStaff

--- backend/patient-ms/src/main/java/com/fullstackdevops/patientms/repository/PatientRepository.java
+++ backend/patient-ms/src/main/java/com/fullstackdevops/patientms/repository/PatientRepository.java
@@ -2,10 +2,13 @@ package com.fullstackdevops.patientms.repository;
 
 import com.fullstackdevops.patientms.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    Optional<Patient> findByUserId(Long userId);
+    @Query("SELECT p FROM Patient p WHERE p.userId = :userId")
+    Optional<Patient> findByUserId(@Param("userId")Long userId);
 
 }
