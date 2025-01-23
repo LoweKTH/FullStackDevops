49b7cc24f7320d58fc3b0e5115f035a14db57285
benle <benle@kth.se>
2025-01-23 15:52:09 +0100

    search ms kthcloud

--- backend/search-ms/src/main/java/com/fullstackdevops/searchms/controller/SearchResource.java
+++ backend/search-ms/src/main/java/com/fullstackdevops/searchms/controller/SearchResource.java
@@ -6,12 +6,10 @@ import com.fullstackdevops.searchms.dto.DoctorWithPatients;
 import com.fullstackdevops.searchms.dto.PatientDto;
 import com.fullstackdevops.searchms.service.SearchService;
 import io.smallrye.mutiny.Uni;
-import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import java.util.List;
 
@@ -22,15 +20,11 @@ public class SearchResource {
 
     private final SearchService searchService;
 
-    @Inject
-    JsonWebToken jwt; // Inject JsonWebToken
+    public SearchResource(SearchService searchService) {this.searchService = searchService;}
 
-    public SearchResource(SearchService searchService) {
-        this.searchService = searchService;
-    }
+
 
     @GET
-    @PermitAll
     @Path("/{patientId}")
     public Response searchDoctorsForPatient(
             @PathParam("patientId") String patientId,
@@ -42,34 +36,22 @@ public class SearchResource {
                     .build();
         }
 
-        // Access JWT claims (example)
-        String username = jwt.getName(); // Get the username
-        System.out.println("User accessing searchDoctorsForPatient: " + username);
-
         DoctorStaffDto result = searchService.searchDoctorsForPatient(patientId, doctorName);
 
         return Response.ok(result).build();
     }
 
     @GET
-    @PermitAll
     @Path("/patients")
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<List<PatientDto>> searchPatientsByDiagnosis(@QueryParam("diagnosis") String diagnosis) {
-        // Access JWT claims (example)
-        String username = jwt.getName(); // Get the username
-        System.out.println("User accessing searchPatientsByDiagnosis: " + username);
         return searchService.searchPatientsByDiagnosis(diagnosis);
     }
 
     @GET
-    @PermitAll
     @Path("/searchDoctors")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<List<DoctorWithPatients>> searchDoctorsWithPatients(@QueryParam("name") String name) {
-        // Access JWT claims (example)
-        String username = jwt.getName(); // Get the username
-        System.out.println("User accessing searchDoctorsWithPatients: " + username);
+    public Uni<List<DoctorWithPatients>> searchDoctorsWithPatients(@QueryParam("name") String name){
         return searchService.getDoctorsWithPatients(name);
     }
 }
